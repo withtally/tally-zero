f417682cf0b3ea4f6da8ab9efcaafaf12ab17a3b
FindMalek <malekgarahellalbus@gmail.com>
2024-02-08 13:32:37 +0100

    :lipstick: Updated: Better Accessibility & UX

--- components/container/DaoCard.tsx
+++ components/container/DaoCard.tsx
@@ -30,21 +30,25 @@ export default function DaoCard({
   return (
     <Card className="rounded-xl my-2">
       <CardContent className="flex items-start space-x-4 py-2">
-        <Image
-          src={imageUrl}
-          alt={name}
-          width={64}
-          height={64}
-          className="rounded-full flex-shrink-0 pt-1 -ml-2"
-        />
+        <a href={`/explore?address=${ethAddress}`}>
+          <Image
+            src={imageUrl}
+            alt={name}
+            width={64}
+            height={64}
+            className="rounded-full flex-shrink-0 pt-1 -ml-2"
+          />
+        </a>
         <div className="flex flex-col justify-center py-2">
           <div className="flex justify-between items-center w-full">
-            <span className="text-lg font-semibold">{name}</span>
+            <a href={`/explore?address=${ethAddress}`}>
+              <span className="text-lg font-semibold">{name}</span>
+            </a>
             <a
               href={`/explore?address=${ethAddress}`}
               className="text-sm text-purple-500 hover:text-purple-600 transition-colors duration-200 ease-in-out flex items-center"
             >
-              Connect to DAO
+              Connect
               <ArrowRight className="w-4 h-4 ml-2" />
             </a>
           </div>
@@ -53,10 +57,15 @@ export default function DaoCard({
             <Card className="rounded-sm">
               <CardDescription
                 onClick={handleCopyClick}
-                className="flex items-center px-2 py-1 space-x-2 hover:text-black transition-colors duration-200 ease-in-out cursor-pointer"
+                className="flex items-center px-2 py-1 space-x-2 hover:text-purple-800 transition-colors duration-200 ease-in-out cursor-pointer"
               >
-                <p className="text-gray-500 font-mono truncate hover:text-purple-500 transition-colors duration-200 ease-in-out">
+                <p className="hidden sm:block text-gray-500 font-mono truncate hover:text-purple-500 transition-colors duration-200 ease-in-out">
                   {ethAddress}
+                </p>
+                <p className="sm:hidden block text-gray-500 font-mono truncate hover:text-purple-500 transition-colors duration-200 ease-in-out">
+                  {`${ethAddress.substring(0, 12)}...${ethAddress.substring(
+                    ethAddress.length - 8
+                  )}`}
                 </p>
                 {isCopied ? (
                   <CheckIcon className="w-4 h-4 text-purple-500" />
